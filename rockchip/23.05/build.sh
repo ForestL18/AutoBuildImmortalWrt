22d83135969f30fa95754877d267838b7b83f996
ForestL18 <45709305+ForestL18@users.noreply.github.com>
2025-03-22 14:46:46 +0800

    modified for own use

--- rockchip/23.05/build.sh
+++ rockchip/23.05/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Log file for debugging
 LOGFILE="/tmp/uci-defaults-log.txt"
-echo "Starting 99-custom.sh at $(date)" >>$LOGFILE
+echo "Starting 99-custom.sh at $(date)" >> $LOGFILE
 # yml 传入的路由器型号 PROFILE
 echo "Building for profile: $PROFILE"
 # yml 传入的固件大小 ROOTFS_PARTSIZE
@@ -10,7 +10,7 @@ echo "Building for ROOTFS_PARTSIZE: $ROOTFS_PARTSIZE"
 echo "Create pppoe-settings"
 
 # 创建pppoe配置文件 yml传入环境变量ENABLE_PPPOE等 写入配置文件 供99-custom.sh读取
-cat <<EOF >/home/build/immortalwrt/files/tmp/pppoe-settings
+cat << EOF > /home/build/immortalwrt/files/tmp/pppoe-settings
 enable_pppoe=${ENABLE_PPPOE}
 pppoe_account=${PPPOE_ACCOUNT}
 pppoe_password=${PPPOE_PASSWORD}
@@ -24,8 +24,8 @@ echo "$(date '+%Y-%m-%d %H:%M:%S') - Starting build process..."
 
 # 下载自定义软件包
 SOFTWARE_VERSION="$(curl -s https://api.github.com/repos/ForestL18/OpenWrt-nikki/releases/latest | grep 'tag_name' | cut -d\" -f4)"
-SOFTWARE_URL="https://github.com/ForestL18/OpenWrt-nikki/releases/download/${SOFTWARE_VERSION}/nikki_aarch64_generic-openwrt-24.10.tar.gz"
-#https://github.com/ForestL18/OpenWrt-nikki/releases/download/v1.21.1/nikki_aarch64_generic-openwrt-24.10.tar.gz
+SOFTWARE_URL="https://github.com/ForestL18/OpenWrt-nikki/releases/download/${SOFTWARE_VERSION}/nikki_aarch64_generic-openwrt-23.05.tar.gz"
+#https://github.com/ForestL18/OpenWrt-nikki/releases/download/v1.21.1/nikki_aarch64_generic-openwrt-23.05.tar.gz
 
 mkdir -p /home/build/immortalwrt/packages
 mkdir -p /tmp/nikki/nikki_unzip
@@ -33,7 +33,7 @@ wget $SOFTWARE_URL -O /tmp/nikki/nikki.tar.gz
 tar xzf /tmp/nikki/nikki.tar.gz -C /tmp/nikki/nikki_unzip
 mv /tmp/nikki/nikki_unzip/*nikki* /home/build/immortalwrt/packages
 
-# 定义所需安装的包列表 24.10 下列插件你都可以自行删减
+# 定义所需安装的包列表 23.05 下列插件你都可以自行删减
 PACKAGES=""
 PACKAGES="$PACKAGES q"
 PACKAGES="$PACKAGES yq"
@@ -51,11 +51,12 @@ PACKAGES="$PACKAGES luci-i18n-base-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-cpufreq-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-diskman-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-firewall-zh-cn"
-PACKAGES="$PACKAGES luci-i18n-package-manager-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-opkg-zh-cn"
 PACKAGES="$PACKAGES autocore"
 PACKAGES="$PACKAGES automount"
 PACKAGES="$PACKAGES base-files"
 PACKAGES="$PACKAGES block-mount"
+PACKAGES="$PACKAGES busybox"
 PACKAGES="$PACKAGES ca-bundle"
 PACKAGES="$PACKAGES default-settings-chn"
 PACKAGES="$PACKAGES dnsmasq-full"
@@ -63,6 +64,7 @@ PACKAGES="$PACKAGES dropbear"
 PACKAGES="$PACKAGES fdisk"
 PACKAGES="$PACKAGES firewall4"
 PACKAGES="$PACKAGES fstools"
+PACKAGES="$PACKAGES ipv6helper"
 PACKAGES="$PACKAGES kmod-gpio-button-hotplug"
 PACKAGES="$PACKAGES kmod-nf-nathelper"
 PACKAGES="$PACKAGES kmod-nf-nathelper-extra"
@@ -71,27 +73,27 @@ PACKAGES="$PACKAGES libc"
 PACKAGES="$PACKAGES libgcc"
 PACKAGES="$PACKAGES libustream-openssl"
 PACKAGES="$PACKAGES logd"
+PACKAGES="$PACKAGES luci"
 PACKAGES="$PACKAGES luci-compat"
 PACKAGES="$PACKAGES luci-lib-base"
 PACKAGES="$PACKAGES luci-lib-ipkg"
-PACKAGES="$PACKAGES luci-light"
 PACKAGES="$PACKAGES mkf2fs"
 PACKAGES="$PACKAGES mtd"
 PACKAGES="$PACKAGES netifd"
 PACKAGES="$PACKAGES nftables"
-PACKAGES="$PACKAGES odhcp6c"
-PACKAGES="$PACKAGES odhcpd-ipv6only"
 PACKAGES="$PACKAGES opkg"
 PACKAGES="$PACKAGES partx-utils"
 PACKAGES="$PACKAGES ppp"
 PACKAGES="$PACKAGES ppp-mod-pppoe"
+PACKAGES="$PACKAGES procd"
+PACKAGES="$PACKAGES procd-seccomp"
 PACKAGES="$PACKAGES procd-ujail"
 PACKAGES="$PACKAGES uboot-envtools"
 PACKAGES="$PACKAGES uci"
 PACKAGES="$PACKAGES uclient-fetch"
 PACKAGES="$PACKAGES urandom-seed"
 PACKAGES="$PACKAGES urngd"
-PACKAGES="$PACKAGES kmod-r8169"
+PACKAGES="$PACKAGES kmod-r8168"
 
 # 构建镜像
 echo "$(date '+%Y-%m-%d %H:%M:%S') - Building image with the following packages:"
